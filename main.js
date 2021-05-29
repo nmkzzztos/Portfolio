5fed97cb7a2fd128b37154d05f4d24d8ce426a07
nmkzzztos <78933262+nmkzzztos@users.noreply.github.com>
2021-05-29 13:28:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function showAllprojects() {
+    document.getElementById('card-1').style.display = "block";
+    document.getElementById('card-2').style.display = "block";
+    document.getElementById('card-3').style.display = "block";
+    document.getElementById('card-4').style.display = "block";
+};
+
+function showWebdesign() {
+    document.getElementById('card-1').style.display = "block";
+    document.getElementById('card-2').style.display = "block";
+    document.getElementById('card-3').style.display = "block";
+    document.getElementById('card-4').style.display = "none";
+};
+
+function showOtherprojects() {
+    document.getElementById('card-1').style.display = "none";
+    document.getElementById('card-2').style.display = "none";
+    document.getElementById('card-3').style.display = "none";
+    document.getElementById('card-4').style.display = "block";
+};
